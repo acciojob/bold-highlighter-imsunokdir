66201e18b092ff0dbbbb839a7e047e1bedc168e5
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-07-24 16:16:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+const boldWords = document.querySelectorAll("strong");
 function highlight() {
     //Write your code here
-
+	boldWords.forEach(word=>word.style.color="green")
 
 }
-
 
 function return_normal() {
     //Write your code here
-
+	boldWords.forEach(word=>word.style.color="")
     
 }
+
+
